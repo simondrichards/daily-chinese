04d73a9bb58f9775942dc24170875bfb1d37d382
Simon Richards <s.richards@physics.org>
2018-09-24 23:17:04 +0100

    Initial Commit

---
+++ daily-chinese/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  daily-chinese
+//
+//  Created by Simon Richards on 24/09/2018.
+//  Copyright © 2018 Simon Richards. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
