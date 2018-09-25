80566a7c59b608a9394cc75db8bfe151a42a2814
Simon Richards <s.richards@physics.org>
2018-09-25 23:06:34 +0100

    Add Lesson view controller with web view of ChinesePod lesson

---
+++ daily-chinese/LessonViewController.swift
@@ -0,0 +1,42 @@
+//
+//  LessonViewController.swift
+//  daily-chinese
+//
+//  Created by Simon Richards on 25/09/2018.
+//  Copyright © 2018 Simon Richards. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class LessonViewController: UIViewController, WKUIDelegate {
+    
+    var webView: WKWebView!
+
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let myURL = URL(string:"https://chinesepod.com/lessons/no-bargaining")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
