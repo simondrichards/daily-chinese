e0e2ffd55dadf10a534f1ff4e2cec021c434b842
Simon Richards <s.richards@physics.org>
2018-09-29 10:34:15 +0100

    Add code to loop through games

--- daily-chinese/PlayViewController.swift
+++ daily-chinese/PlayViewController.swift
@@ -10,18 +10,26 @@
 
 import UIKit
 
+var games = ["game a", "game b", "game c"]
 var current_game_score: Int = 8
+
+var current_game: Int = 0
 
 class PlayViewController: UIViewController {
     
 
     @IBOutlet weak var currentGameScore: UILabel!
+    @IBOutlet weak var currentGame: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        currentGame.text = games[current_game]
         currentGameScore.text = String(current_game_score)
+        
+        current_game = current_game + 1
+        if current_game == games.count {current_game=0}
     }
     
 
